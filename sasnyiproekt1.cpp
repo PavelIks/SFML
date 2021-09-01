2319aaf49c4bb812c3904b7b24b4bd5f1863484b
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-01 13:15:42 +0300

    Updated-1.

--- sasnyiproekt1.cpp
+++ sasnyiproekt1.cpp
@@ -10,7 +10,7 @@ struct S_Point
 
 int main()
 {
-	RenderWindow window(VideoMode(400, 400), "TEST APP");
+	RenderWindow window(VideoMode(1080, 720), "TEST APP");
 	S_Point circle_pos(1, 1);
 
 	while (window.isOpen())
@@ -25,19 +25,20 @@ int main()
 			}
 			if (event.type == Event::KeyPressed)
 			{
-				if (event.key.code == Keyboard::D)
+				// Вправо
+				/*if (event.key.code == Keyboard::D)
 				{
 					circle_pos.x++;
-				}
-				if (event.key.code == Keyboard::S)
+				}*/
+				// Вниз
+				/*if (event.key.code == Keyboard::S)
 				{
 					circle_pos.y++;
-				}
+				}*/
 			}
-
 		}
 
-		CircleShape circle(50.0f);
+		/*CircleShape circle(50.0f);
 		circle.setFillColor(Color(230, 0, 120));
 		circle.setOutlineThickness(2);
 		circle.setOutlineColor(Color(0, 0, 0));
@@ -45,6 +46,27 @@ int main()
 		circle.move(circle_pos.x, circle_pos.y);
 
 		window.draw(circle);
+		window.display();*/
+
+		CircleShape circle(50.f);
+		circle.setFillColor(Color(230, 0, 230));
+		circle.move((1080-100)/2, (720-100)/2);
+		window.draw(circle);
+
+		ConvexShape convex1;
+		window.draw(convex1);
+		RectangleShape rectangle1(Vector2f(20.f, 100.f)); // ВЫСОТА И ШИРИНА ПРЯМОУГОЛЬНИКА
+		rectangle1.move((1080 - 1000) / 2, (720 - 100) / 2); // КООРДИНАТЫ РАСПОЛОЖЕНИЯ ПРЯМОУГОЛЬНИКА
+		rectangle1.setFillColor(Color(0, 0, 0)); // ЦВЕТ ПРЯМОУГОЛЬНИКА
+		window.draw(rectangle1);
+
+		ConvexShape convex2;
+		window.draw(convex2);
+		RectangleShape rectangle2(Vector2f(20.f, 100.f)); // ВЫСОТА И ШИРИНА ПРЯМОУГОЛЬНИКА
+		rectangle2.move((1080 + 990) / 2, (720 - 100) / 2); // КООРДИНАТЫ РАСПОЛОЖЕНИЯ ПРЯМОУГОЛЬНИКА
+		rectangle2.setFillColor(Color(0, 0, 0)); // ЦВЕТ ПРЯМОУГОЛЬНИКА
+		window.draw(rectangle2);
+
 		window.display();
 	}
 }
